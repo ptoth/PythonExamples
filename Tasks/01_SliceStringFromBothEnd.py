180cbe3c7d1691729fb46770e3ed94a59004160d
Péter György Tóth <ptoth@users.noreply.github.com>
2022-02-09 21:40:00 +0100

    Task upload
    Added two simple tasks

---
+++ Tasks/01_SliceStringFromBothEnd.py
@@ -0,0 +1,39 @@
+print("Simple code for showing slicing of a string")
+print("===== START =====")
+
+myText = "This text is a sample"
+
+print("Our sample text is: "+myText)
+
+# now we are slicing  every character from the end:
+print("Slice characters from the end:")
+print(myText[0:len(myText)])
+print(myText[0:len(myText)-1])
+print(myText[0:len(myText)-2])
+print(myText[0:len(myText)-3])
+print(myText[0:len(myText)-4])
+print(myText[0:len(myText)-5])
+print(myText[0:len(myText)-6])
+print(myText[0:len(myText)-7])
+print(myText[0:len(myText)-8])
+print(myText[0:len(myText)-9])
+print(myText[0:len(myText)-10])
+
+print("\n")
+
+
+# now we are slicing from the other direction:
+print("Slice characters from the start:")
+print(myText[0:len(myText)])
+print(myText[1:len(myText)])
+print(myText[2:len(myText)])
+print(myText[3:len(myText)])
+print(myText[4:len(myText)])
+print(myText[5:len(myText)])
+print(myText[6:len(myText)])
+print(myText[7:len(myText)])
+print(myText[8:len(myText)])
+print(myText[9:len(myText)])
+print(myText[10:len(myText)])
+
+print("===== STOP =====")
---
+++ Tasks/02_ReplaceLetterAwithE.py
@@ -0,0 +1,17 @@
+print("Simple code for character replace in a string")
+print("===== START =====")
+
+myText = "This text is a sample"
+
+print("Our sample text is: "+myText)
+
+print(myText.replace("a","e"))
+
+# reminder: replacing does not overwrite the original variable
+print(myText)
+
+# but this will:
+myText = myText.replace("a","e")
+print(myText)
+
+print("===== STOP =====")
