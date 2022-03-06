abed0be17e571ad717511abb5b279974a9c2db32
ptoth <ptoth@users.noreply.github.com>
2022-03-06 19:35:04 +0100

    Create 05_calculator.py

---
+++ Tasks/05_calculator.py
@@ -0,0 +1,20 @@
+print("START")
+firstNum = input("Provide first number: ")
+secondNum = input("Provide second number: ")
+operator = input("Provide operator: ")
+
+if (operator == "+"):
+    print("Result incorrect: ", firstNum + secondNum) 
+    print("Result correct (casted as float type): ", float(firstNum) + float(secondNum)) 
+elif (operator == "-"):
+    print("Result: ", firstNum - secondNum)
+    print("Result correct (casted as float type): ", float(firstNum) - float(secondNum)) 
+elif (operator == "*"):
+    print("Result incorrect: ", firstNum * secondNum)
+    print("Result correct, but wrong casting: ", int(firstNum) * int(secondNum))
+    print("Result correct, with proper castong: ", float(firstNum) * float(secondNum))
+elif (operator == "/"):
+    print("Result incorrect: ", int(firstNum) / int(secondNum))
+
+
+
