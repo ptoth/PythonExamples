7c1419ccbc79576283522de662b9cf1570255c7f
ptoth <ptoth@users.noreply.github.com>
2023-12-30 13:32:49 +0100

    Update 08_InviteList.py

--- Practice/08_InviteList.py
+++ Practice/08_InviteList.py
@@ -1,12 +1,11 @@
 numOfGuests = int(input(("Please provide, how many guests you are expecting:")))
 guestList = []
-for i in range(numOfGuests):
-    guestList.append(input("Please provide the name of the guest #"+str(i+1)+":"))
 
 menuIndex = -1
 while (menuIndex != 9):
+    print("==============================")
     print("Please choose from the menu:")
-    print("1. add guest")
+    print("1. add guest to the end of the list")
     print("2. remove guest")
     print("3. list guests in order of addition")
     print("4. list guests in alphabetical order")
@@ -17,10 +16,15 @@ while (menuIndex != 9):
         newGuest = input("Please provide the guest' name: ")
         guestList.append(newGuest)
 
-    if (menuIndex == 3): 
-        print(guestList)
+    if (menuIndex == 3):
+        if guestList:
+            print(guestList)
+        else:
+            print("The list is empty!")
         
     if (menuIndex == 4):
-        guestList.sort()
-        print(guestList)
+        # To avoid permanent change, using sorted
+        print(sorted(guestList))
     
+    if (menuIndex == 5):
+        pass
